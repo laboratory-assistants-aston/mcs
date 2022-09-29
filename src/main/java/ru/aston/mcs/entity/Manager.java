4200cb3e41a3a674dd695da2955cfa4bb6fd3950
Alexander Latyshev <imposya0@gmail.com>
2022-09-29 16:52:23 +0300

    {mcs}{LA-1}{created 3 entities}

---
+++ src/main/java/ru/aston/mcs/entity/Manager.java
@@ -0,0 +1,43 @@
+package ru.aston.mcs.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "customers")
+@Entity
+public class Manager {
+    @Id
+    @Column(name = "manager_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "role_id")
+    @OneToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(name = "resources_id")
+    @OneToMany
+    @JoinColumn(name = "manager_id")
+    private List<Resource> resourceList;
+
+    @Column(name = "users_id")
+    @OneToMany
+    @JoinColumn(name = "manager_id")
+    private List<User> userList;
+
+    @Column(name = "email")
+    private String email;
+}
