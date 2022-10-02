dff9f897f7c9b227c3b7af65d4134e21e4317fa6
Alexander Latyshev <imposya0@gmail.com>
2022-10-02 05:34:31 +0300

    TASK-12: Deleted Housekeeper

--- src/main/java/ru/aston/mcs/repository/HousekeeperRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.aston.mcs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.aston.mcs.entity.Housekeeper;
-
-@Repository
-public interface HousekeeperRepository extends JpaRepository<Housekeeper, Long> {
-}
