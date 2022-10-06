8674a4f235aac4d690ea66af74d5c65811862c87
Knoxa <84104571+DamirDelmeev@users.noreply.github.com>
2022-10-06 15:45:58 +0300

    add crud test for ResidentController

--- src/main/java/ru/aston/mcs/controller/ResidentController.java
+++ src/main/java/ru/aston/mcs/controller/ResidentController.java
@@ -1,13 +1,7 @@
 package ru.aston.mcs.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.aston.mcs.dto.ResidentDTO;
 import ru.aston.mcs.service.ResidentService;
 
@@ -34,17 +28,20 @@ public class ResidentController {
     }
 
     @PostMapping("/")
-    public void saveResident(@RequestBody ResidentDTO residentDto) {
+    public ResponseEntity<Void> saveResident(@RequestBody ResidentDTO residentDto) {
         residentService.createResident(residentDto);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/")
-    public void updateResident(@RequestBody ResidentDTO residentDto) {
+    public ResponseEntity<Void> updateResident(@RequestBody ResidentDTO residentDto) {
         residentService.createResident(residentDto);
+        return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/")
-    public void deleteResident(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteResident(@PathVariable Long id) {
         residentService.deleteResident(id);
+        return ResponseEntity.ok().build();
     }
 }
