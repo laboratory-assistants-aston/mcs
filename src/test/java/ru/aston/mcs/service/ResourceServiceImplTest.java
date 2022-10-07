91d787f27133414669afc6f7245c6ba253ad51cd
Knoxa <84104571+DamirDelmeev@users.noreply.github.com>
2022-10-08 00:04:04 +0300

    LAA-35 added test for Resource

---
+++ src/test/java/ru/aston/mcs/service/ResourceServiceImplTest.java
@@ -0,0 +1,62 @@
+package ru.aston.mcs.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.aston.mcs.dto.ResourceDTO;
+import ru.aston.mcs.mapper.ResourceMapper;
+import ru.aston.mcs.repository.ResourceRepository;
+import ru.aston.mcs.service.impl.ResourceServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class ResourceServiceImplTest {
+    @Mock
+    private ResourceRepository resourceRepository;
+
+    @InjectMocks
+    private ResourceServiceImpl resourceServiceImpl;
+    @Mock
+    ResourceMapper resourceMapper;
+
+    private ResourceDTO resourceDTO;
+
+    @BeforeEach
+    void createDto() {
+        resourceDTO = new ResourceDTO();
+        resourceDTO.setId(0L);
+        resourceDTO.setResourceIdentifier(0);
+        resourceDTO.setResourceType(null);
+        resourceDTO.setBookingResourceList(null);
+    }
+
+    @Test
+    void getAllResourceTest() {
+        resourceServiceImpl.getAllResource();
+        Mockito.verify(resourceRepository).findAll();
+    }
+
+    @Test
+    void getResourceByIdTest() {
+        RuntimeException runtimeException = assertThrows(RuntimeException.class,
+                () -> resourceServiceImpl.getResource(0L));
+        Mockito.verify(resourceRepository).findById(1L);
+    }
+
+    @Test
+    void createResourceFromDtoTest() {
+        resourceServiceImpl.addAndSaveResource(resourceDTO);
+        Mockito.verify(resourceRepository).save(resourceMapper.toModel(resourceDTO));
+    }
+
+    @Test
+    void deleteResourceByIdTest() {
+        resourceServiceImpl.deleteResource(1L);
+        Mockito.verify(resourceRepository).deleteById(1L);
+    }
+}
